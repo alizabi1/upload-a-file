727259c0d19b0b20200d4005d0e440903d60f406
Alicja <alicja@ymail.com>
2025-11-04 17:25:19 +0000

    Pom updated

--- src/test/java/com/example/demo/controller/FileControllerTest.java
+++ src/test/java/com/example/demo/controller/FileControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
